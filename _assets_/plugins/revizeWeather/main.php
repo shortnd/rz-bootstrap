287d2f696b72c9488dd4489acd98e61fcd2b20fd
Collin OConnell <ococncol@gmail.com>
2019-04-15 16:08:34 -0400

    updated weather pluggin

--- _assets_/plugins/revizeWeather/main.php
+++ _assets_/plugins/revizeWeather/main.php
@@ -2,6 +2,7 @@
 // error_reporting(E_ALL);ini_set('display_errors', 1);
 require_once __DIR__."/class/OpenWeatherMapWidget.php";
 require_once __DIR__."/config.php";
+
 header('Content-Type: application/json; charset=UTF-8');
 
 /******************
@@ -24,6 +25,6 @@ try {
 	print $response;
 
 } catch (Exception $e) {
-	$response = array( "error" => "$e->getMessage()");
+	$response = array( "error" => $e->getMessage());
 	print json_encode($response);
 }
